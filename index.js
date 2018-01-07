d3ca4fa6802c7a68856777ada6f599767eee0ddb
andyak47321 <andyak47321@gmail.com>
2018-01-07 19:48:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: 1556012053,
-  channelSecret: 3a70dde3e90c9feca19b79b68ed63212,
-  channelAccessToken: 20gDnqxjSvvwaglT5bkj4hFma3ZQ0rtPbyETaiP/Qoy/R13WEy6xvxtxDBluA47R2wA8TJ/NU1+nAea97bDxO55K4G1pf6L9dDIwGOmUaC+y70J4mZwB+6uFQEzGYulw2cJ7yRdjfizAutyM6E4bZgdB04t89/1O/w1cDnyilFU=
+  channelId: ,
+  channelSecret: ,
+  channelAccessToken: 
 });
 
 bot.on('message', function(event) {
